e05e9dfed8f992ffa7549d20cd908d0ed7273404
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-05-02 14:26:10 -0300

    Utilizando padrão singleton para mesma instância no controlador e no convert

--- src/main/java/br/edu/ifpb/infra/memory/PessoasEmMemoria.java
+++ src/main/java/br/edu/ifpb/infra/memory/PessoasEmMemoria.java
@@ -11,49 +11,62 @@ import java.util.List;
  */
 public class PessoasEmMemoria implements Pessoas {
 
-    private final List<Pessoa> pessoas = new ArrayList<>();
+	private final List<Pessoa> pessoas = new ArrayList<>();
 
-    public void nova(Pessoa pessoa) {
-    	pessoas.add(pessoa);
-    }
+	private static PessoasEmMemoria instance = null;
 
-    public List<Pessoa> todas() {
-        return pessoas;
-    }
-    
+	public static PessoasEmMemoria getInstance() {
+		if (instance == null) {
+			synchronized (PessoasEmMemoria.class) {
+				if (instance == null) {
+					instance = new PessoasEmMemoria();
+				}
+			}
+		}
+		return instance;
+	}
+
+	public void nova(Pessoa pessoa) {
+		pessoas.add(pessoa);
+	}
+
+	public List<Pessoa> todas() {
+		return pessoas;
+	}
+
 	@Override
 	public List<Pessoa> buscarPorCPF(String cpf) {
 		List<Pessoa> resultadoBusca = new ArrayList<>();
-		for(Pessoa p: pessoas) {
+		for (Pessoa p : pessoas) {
 //			faz um substring em todos os cpf cadastrados para deixar do mesmo tamanho
 //			que o usuário digitou, e compara para ver se é igual;
 			String comparar = p.getCpf().valor().substring(0, cpf.length());
-			if(comparar.equals(cpf)) {
+			if (comparar.equals(cpf)) {
 				resultadoBusca.add(p);
 			}
 		}
 		return resultadoBusca;
 	}
 
-    public void excluir(Pessoa pessoa) {
-        pessoas.remove(pessoa);
-    }
+	public void excluir(Pessoa pessoa) {
+		pessoas.remove(pessoa);
+	}
 
-    public void atualizar(Pessoa pessoa) {
-        pessoas.remove(pessoa);
-        pessoas.add(pessoa);
-    }
+	public void atualizar(Pessoa pessoa) {
+		pessoas.remove(pessoa);
+		pessoas.add(pessoa);
+	}
 
-    @Override
-    public List<Dependente> todosOsDepentendes() {
-        //TODO
-    	return null;
-    }
+	@Override
+	public List<Dependente> todosOsDepentendes() {
+		// TODO
+		return null;
+	}
 
-    @Override
-    public Dependente localizarDependenteComId(String uuid) {
-        //TODO: implementar
-    	return null;
-    }
+	@Override
+	public Dependente localizarDependenteComId(String uuid) {
+		// TODO: implementar
+		return null;
+	}
 
 }
