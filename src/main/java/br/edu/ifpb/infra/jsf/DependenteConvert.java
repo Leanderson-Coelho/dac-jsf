672f230afe4eb9f9c91f6fecca60efed6fcc574b
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-05-02 14:24:49 -0300

    Refatorando método getAsString

--- src/main/java/br/edu/ifpb/infra/jsf/DependenteConvert.java
+++ src/main/java/br/edu/ifpb/infra/jsf/DependenteConvert.java
@@ -8,20 +8,32 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 import br.edu.ifpb.domain.Dependente;
+import br.edu.ifpb.infra.memory.DependentesEmMemoria;
 
 @FacesConverter(value="convert.dependente")
 public class DependenteConvert implements Converter{
 
+//	busca a instancia da lista de dependentes
+	private DependentesEmMemoria instance = DependentesEmMemoria.getInstance();
+	
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
-		Dependente dep = new Dependente(value,LocalDate.now());	
-		return null;
+//		verifica se foi selecionado um dependente
+		if(value.equals("-- NENHUM DEPENDENTE --")) {
+			return null;
+		}
+		Dependente dep = instance.localizarDependenteComId(value);
+		return dep;
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
-		// TODO Auto-generated method stub
-		return null;
+//		verifica se foi selecionado um dependente
+		if(value==null) {
+			return null;
+		}
+		Dependente dep = (Dependente) value;
+		return dep.getUuid();
 	}
 
 }
